9967c4fa2afdfeb394f78171d10dfa4b6f411447
Jonathan Haines <jonno.haines@gmail.com>
2018-04-21 17:23:26 +1000

    actual local development

--- config/default.js
+++ config/default.js
@@ -5,6 +5,6 @@ module.exports = {
   API_CLIENT_URL: '',
   API_SERVER_URL: deferConfig(config => `http://localhost:${config.port}`),
   COOKIE_SECRET: 'focustas',
-  MONGO_URI: 'mongodb://localhost/focustas',
+  MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost/focustas',
   dev: !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
 };
